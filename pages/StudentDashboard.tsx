f00aa92a1e4ad0206b2b2001108d11fd6dd82e7f
HavocProtocol <tmpyoutub204@gmail.com>
2026-02-04 10:05:18 +0300

    Update StudentDashboard.tsx

--- pages/StudentDashboard.tsx
+++ pages/StudentDashboard.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { 
@@ -298,3 +297,4 @@ export const StudentDashboard: React.FC = () => {
       )}
     </div>
   );
+};
